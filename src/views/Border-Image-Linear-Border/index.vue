1e3ea900d2aa357c03b7ad6d2654a7ea6d9108ba
kirito <kiritoxjf@outlook.com>
2024-10-29 16:04:36 +0800

    边框渐变背景图

---
+++ src/views/Border-Image-Linear-Border/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-box">
+    <div class="content text-2xl px-4 py-2 bg-blue-dark border-2 border-transparent">
+      Content
+    </div>
+  </div>
+</template>
+<style scoped lang="less">
+.content {
+  border-image: linear-gradient(153deg, #114357, #f29492) 1;
+}
+</style>
---
+++ src/views/Border-Image-Linear-Border/page.ts
@@ -0,0 +1,9 @@
+import type { iPage } from '@/interface/common'
+
+const page: iPage = {
+  level: 1,
+  name: 'Border-Image-Linear-Border',
+  title: '边框图片渐变边框'
+}
+
+export default page
